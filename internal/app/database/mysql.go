6e9ff7ba56659a197ebe12b86c581fedad87b806
Sigit Ispramono H <87635612+sasyua26@users.noreply.github.com>
2022-11-26 21:12:01 +0700

    Update mysql.go

--- internal/app/database/mysql.go
+++ internal/app/database/mysql.go
@@ -1,13 +1,34 @@
 package database
 
 import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 // Function to connect golang to online mysql server at freesqldatabase.com
 func NewConnDatabase() *gorm.DB {
-	dsn := "sql6580832:zwTxNEJj5r@tcp(sql6.freesqldatabase.com:3306)/sql6580832?charset=utf8mb4&parseTime=True&loc=Local"
+	err := godotenv.Load(".env")
+	if err != nil {
+		err = godotenv.Load()
+		if err != nil {
+			log.Fatalf("Error loading .env file")
+		}
+	}
+
+	//dsn := "sql6580832:zwTxNEJj5r@tcp(sql6.freesqldatabase.com:3306)/sql6580832?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf(
+		"%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		os.Getenv("USER"),
+		os.Getenv("PASSWORD"),
+		os.Getenv("HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DATABASE"),
+	)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err)
